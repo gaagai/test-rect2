44fb183a54d3fe0b3eeef069ed33b43271ca4acc
Gal Agai <gal@nbv.co.il>
2020-01-16 16:24:01 +0200

    LAST  BUILD

---
+++ client/build/precache-manifest.2bbc158d9cddd0a319c2c0df0cf438e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236348cd0ff40588073ad71ffb2fbf5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2df459b7dd0ac0e85c6",
+    "url": "/static/css/main.77bc839d.chunk.css"
+  },
+  {
+    "revision": "29488887a5142f771ce6",
+    "url": "/static/js/2.11f231dc.chunk.js"
+  },
+  {
+    "revision": "bbddf3125262f23363e1a0a0e553fdc7",
+    "url": "/static/js/2.11f231dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2df459b7dd0ac0e85c6",
+    "url": "/static/js/main.26978ef6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
